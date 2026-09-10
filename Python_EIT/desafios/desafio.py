f6d0039c85c5c3770ed6ac750fe3bb1927d12c65
Damián Nicolás Alderete <damian.aldert@gmail.com>
2026-09-10 12:17:38 -0300

    creacion de la carpeta Desafios

---
+++ Python_EIT/desafios/desafio.py
@@ -0,0 +1,9 @@
+alumnoNota = int(input('Hola Alumno ingresa tu nota: '))
+if (alumnoNota == 10):
+    print('Excelente Nota')
+elif (alumnoNota >7 and alumnoNota <9):
+    print('Muy bien')
+elif (alumnoNota > 4 and alumnoNota <6):
+    print('Mal')
+else:
+    print('La nota ingresada es INCORRECTA!!')
